797d8cc5841e8df3c9dda006c9880df735c978ad
mklevin <mkl@udel.edu>
2015-04-11 16:19:31 -0400

    Made branch

--- WhatsUpSpaceApps/app.js
+++ WhatsUpSpaceApps/app.js
@@ -1,4 +1,6 @@
-﻿var express = require('express');
+﻿// boilerplate
+
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
